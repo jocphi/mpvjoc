99aa1c04e02741dafc3aaa2c1bd03c61ac903a49
jocphi <joc.phi@gmail.com>
2026-07-05 13:13:46 +0200

    Stabilize current mpvjoc feature set

--- main.cpp
+++ main.cpp
@@ -3,6 +3,11 @@
 #include <QStringList>
 #include "ui/MainWindow.h"
 #include "util/Utils.h"
+#include <QCoreApplication>
+#include <QDir>
+#include <QFileInfo>
+#include <QGuiApplication>
+#include <QIcon>
 
 int main(int argc,char**argv){
     forceCLocaleForMpv();
@@ -12,6 +17,26 @@ int main(int argc,char**argv){
     fmt.setVersion(3,3);
     QSurfaceFormat::setDefaultFormat(fmt);
     QApplication app(argc,argv);
+    QCoreApplication::setApplicationName(QStringLiteral("mpvjoc"));
+    QCoreApplication::setOrganizationName(QStringLiteral("mpvjoc"));
+    QGuiApplication::setDesktopFileName(QStringLiteral("mpvjoc"));
+
+    QIcon mpvjocIcon;
+    const QStringList mpvjocIconCandidates = {
+        QDir(QCoreApplication::applicationDirPath()).filePath(QStringLiteral("../assets/mpvjoc.svg")),
+        QDir::current().filePath(QStringLiteral("assets/mpvjoc.svg")),
+        QDir::home().filePath(QStringLiteral(".local/share/icons/hicolor/scalable/apps/mpvjoc.svg")),
+        QStringLiteral("/usr/share/icons/hicolor/scalable/apps/mpvjoc.svg")
+    };
+    for (const QString& iconPath : mpvjocIconCandidates) {
+        if (QFileInfo::exists(iconPath)) {
+            mpvjocIcon = QIcon(iconPath);
+            break;
+        }
+    }
+    if (!mpvjocIcon.isNull())
+        QApplication::setWindowIcon(mpvjocIcon);
+
     forceCLocaleForMpv();
     MainWindow w;
     w.show();
